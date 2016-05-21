9a6febe13e5bce1e9979b765619bb1c25946c21f
Lorenzo Donini <lorenzo.donini@tum.de>
2016-05-22 00:11:52 +0200

    Edit MainActivity with list view logic. Still needs to be mocked properly.

--- app/src/main/java/de/tum/ecorp/reservationapp/MainActivity.java
+++ app/src/main/java/de/tum/ecorp/reservationapp/MainActivity.java
@@ -8,8 +8,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import de.tum.ecorp.reservationapp.model.Restaurant;
+import de.tum.ecorp.reservationapp.model.Review;
+import de.tum.ecorp.reservationapp.view.RestaurantArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView restaurantListView;
+    private ArrayAdapter<Restaurant> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,29 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        restaurantListView = (ListView) findViewById(R.id.listView);
+
+        //Creating adapter
+        listAdapter = new RestaurantArrayAdapter(this, R.layout.restaurant_list_item, getMockRestaurants());
+        //TODO: Add async mechanism to add restaurants (we are fetching them from a server)
+        restaurantListView.setAdapter(listAdapter);
+    }
+
+    private List<Restaurant> getMockRestaurants() {
+        //Mock values
+        List<Review> reviews = new ArrayList<>();
+        reviews.add(new Review("Trololol, bad waiters", 2));
+        reviews.add(new Review("This weeks' product owner sucks :)", 4));
+        Restaurant sample1 = new Restaurant("ECorp creepy restaurant", "Nerdy restaurant",3.1f,reviews);
+        reviews = new ArrayList<>();
+        reviews.add(new Review("Best restaurant ever", 5));
+        reviews.add(new Review("Cookies! Om nom nom..", 4));
+        Restaurant sample2 = new Restaurant("America Graffiti", "American Diner restaurant",1.8f,reviews);
+
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(sample1);
+        restaurants.add(sample2);
+        return restaurants;
     }
 
     @Override
