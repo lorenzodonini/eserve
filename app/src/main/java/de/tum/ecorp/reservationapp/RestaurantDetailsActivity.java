e31450dc6dc8697319865437754427fea7d4e283
Lorenzo Donini <lorenzo.donini@tum.de>
2016-05-26 14:22:37 +0200

    Add restaurant detail activity (layout + menu + activity class)
    Update manifest accordingly

---
+++ app/src/main/java/de/tum/ecorp/reservationapp/RestaurantDetailsActivity.java
@@ -0,0 +1,142 @@
+package de.tum.ecorp.reservationapp;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+import de.tum.ecorp.reservationapp.model.Restaurant;
+import de.tum.ecorp.reservationapp.view.RestaurantDetailFragment;
+import de.tum.ecorp.reservationapp.view.RestaurantReviewsFragment;
+
+public class RestaurantDetailsActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+    private Restaurant mRestaurant;
+
+    private static final String DETAILS_TAB = "Details";
+    private static final String REVIEWS_TAB = "Reviews";
+    private static final String RESERVATION_TAB = "Reservation";
+    public static final String ARG_RESTAURANT = "restaurant";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle arguments = getIntent().getExtras();
+        if (arguments != null) {
+            if (arguments.containsKey(ARG_RESTAURANT)) {
+                mRestaurant = (Restaurant) arguments.get(ARG_RESTAURANT);
+            }
+        }
+
+        setContentView(R.layout.activity_restaurant_details);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (mRestaurant != null) {
+            getSupportActionBar().setTitle(mRestaurant.getName());
+        }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_restaurant_details, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            switch (position) {
+                case 0:
+                    return RestaurantDetailFragment.newInstance(mRestaurant);
+                default:
+                    return RestaurantReviewsFragment.newInstance(mRestaurant.getReviews(), mRestaurant.getRating());
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return DETAILS_TAB;
+                case 1:
+                    return REVIEWS_TAB;
+                case 2:
+                    return RESERVATION_TAB;
+            }
+            return null;
+        }
+    }
+}
