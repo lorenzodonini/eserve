6f07b67a27170776f88541dda53fc43a18384dce
Lorenzo Donini <lorenzo.donini@tum.de>
2016-05-26 14:16:32 +0200

    Make Review model class implement Parcelable

--- app/src/main/java/de/tum/ecorp/reservationapp/model/Review.java
+++ app/src/main/java/de/tum/ecorp/reservationapp/model/Review.java
@@ -1,6 +1,9 @@
 package de.tum.ecorp.reservationapp.model;
 
-public class Review extends Entity {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Review extends Entity implements Parcelable {
     private String reviewText;
     private float rating;
 
@@ -15,5 +18,34 @@ public class Review extends Entity {
 
     public float getRating() {
         return rating;
+    }
+
+    private Review(Parcel in) {
+        reviewText = in.readString();
+        rating = in.readFloat();
+    }
+
+    //PARCELABLE IMPLEMENTATION
+    public static final Creator<Review> CREATOR = new Creator<Review>() {
+        @Override
+        public Review createFromParcel(Parcel in) {
+            return new Review(in);
+        }
+
+        @Override
+        public Review[] newArray(int size) {
+            return new Review[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(reviewText);
+        dest.writeFloat(rating);
     }
 }
