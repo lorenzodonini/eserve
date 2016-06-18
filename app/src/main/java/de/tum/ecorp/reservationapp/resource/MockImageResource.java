88ae76f78b0f9dc02f7863f5a7e7f3308615e3c6
Alexander Lill <alexander.lill@tum.de>
2016-06-18 17:52:16 +0200

    Added more example pictures

--- app/src/main/java/de/tum/ecorp/reservationapp/resource/MockImageResource.java
+++ app/src/main/java/de/tum/ecorp/reservationapp/resource/MockImageResource.java
@@ -63,10 +63,22 @@ public class MockImageResource {
     private Map<Long, List<String>> initializeMockRestaurantImages() {
         Map<Long, List<String>> result = new HashMap<>();
 
-        List<String> list = new ArrayList<>();
+        List<String> list;
 
-        list.add("http://www.nandos.com/sites/all/themes/nandos/images/restaurants/restaurant-carousel-1.jpg");
+        list = new ArrayList<>();
+        list.add("https://www.omnihotels.com/-/media/images/hotels/homrst/restaurants/homrst-omni-homestead-resort-casino-restaurant.jpg");
+        list.add("http://lxly7dz9m3-flywheel.netdna-ssl.com/wp-content/uploads/2015/06/2hawks.jpg");
         result.put(1l, list);
+
+        list = new ArrayList<>();
+        list.add("http://kingofwallpapers.com/restaurant/restaurant-010.jpg");
+        list.add("https://upload.wikimedia.org/wikipedia/commons/1/1e/Tom's_Restaurant,_NYC.jpg");
+        result.put(2l, list);
+
+        list = new ArrayList<>();
+        list.add("http://newyamya.indyco.net/admin/uploaded_image/11037_P1010505.JPG");
+        list.add("http://restaurant-la-cucaracha-mex-bar.mux.de/images/1500x1200z/client/59228/86d6ecu4pv35/restaurant-bar-la-cucaracha-mex-bar-7.jpg");
+        result.put(3l, list);
 
         return result;
     }
