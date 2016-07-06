1a8407891d001799a917b05d16f5d9cb870ee489
Lorenzo Donini <lorenzo.donini@tum.de>
2016-07-06 10:33:34 +0200

    Make drawing function private in the horizontal divider class

--- app/src/main/java/de/tum/ecorp/reservationapp/view/HorizontalDividerItemDecoration.java
+++ app/src/main/java/de/tum/ecorp/reservationapp/view/HorizontalDividerItemDecoration.java
@@ -27,7 +27,7 @@ public class HorizontalDividerItemDecoration extends RecyclerView.ItemDecoration
         drawVertical(c, parent);
     }
 
-    public void drawVertical(Canvas c, RecyclerView parent) {
+    private void drawVertical(Canvas c, RecyclerView parent) {
         final int left = parent.getPaddingLeft();
         final int right = parent.getWidth() - parent.getPaddingRight();
 
@@ -38,22 +38,6 @@ public class HorizontalDividerItemDecoration extends RecyclerView.ItemDecoration
                     .getLayoutParams();
             final int top = child.getBottom() + params.bottomMargin;
             final int bottom = top + mDivider.getIntrinsicHeight();
-            mDivider.setBounds(left, top, right, bottom);
-            mDivider.draw(c);
-        }
-    }
-
-    public void drawHorizontal(Canvas c, RecyclerView parent) {
-        final int top = parent.getPaddingTop();
-        final int bottom = parent.getHeight() - parent.getPaddingBottom();
-
-        final int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {
-            final View child = parent.getChildAt(i);
-            final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
-                    .getLayoutParams();
-            final int left = child.getRight() + params.rightMargin;
-            final int right = left + mDivider.getIntrinsicHeight();
             mDivider.setBounds(left, top, right, bottom);
             mDivider.draw(c);
         }
