18b26ab4c2761ceb0995061c3ebac8e4494e8654
Lorenzo Donini <lorenzo.donini@tum.de>
2016-07-06 09:12:49 +0200

    Add generic ClickListener interface for recycler views

---
+++ app/src/main/java/de/tum/ecorp/reservationapp/view/ClickListener.java
@@ -0,0 +1,8 @@
+package de.tum.ecorp.reservationapp.view;
+
+import android.view.View;
+
+public interface ClickListener {
+    void onClick(View view, int position);
+    void onLongClick(View view, int position);
+}
